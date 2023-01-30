1af3aef8b938a8955616b351d5cf4d2f584c78f5
Rhuan Costanzo <rhuan@cooxupe.com>
2023-01-30 16:49:39 -0300

    feat: rabbitMQ

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,7 +3,10 @@ import {
   Controller,
   Get,
   Logger,
+  Param,
   Post,
+  Put,
+  Query,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
@@ -12,6 +15,9 @@ import {
   Transport,
   ClientProxyFactory,
 } from '@nestjs/microservices';
+import { get } from 'http';
+import { Observable } from 'rxjs';
+import { AtualizarCategoriaDto } from './dtos/atualizar-categoria.dto';
 import { CriarCategoriaDto } from './dtos/criar-categoria.dto';
 
 @Controller('api/v1')
@@ -19,12 +25,16 @@ export class AppController {
   private logger = new Logger(AppController.name);
   private clientAdminbackend: ClientProxy;
 
+  async onApplicationBootstrap() {
+    await this.clientAdminbackend.connect();
+  }
+
   constructor() {
     //injeção de dependência pelo construtor
     this.clientAdminbackend = ClientProxyFactory.create({
       transport: Transport.RMQ,
       options: {
-        urls: ['amqp://user:rgeyrgheg@172.16.0.0:5672/smartranking'],
+        urls: ['amqp://guest:guest@localhost:5672/smartranking'],
         queue: 'admin-backend',
       },
     });
@@ -32,11 +42,30 @@ export class AppController {
 
   @Post('categorias')
   @UsePipes(ValidationPipe)
-  async CriarCategoriaDto(@Body() criarCategoriaDto: CriarCategoriaDto) {
+  CriarCategoriaDto(@Body() criarCategoriaDto: CriarCategoriaDto) {
     //enviando mensagem para o topico
-    return await this.clientAdminbackend.emit(
-      'criar-categoria',
-      criarCategoriaDto,
+    this.clientAdminbackend.emit('criar-categoria', criarCategoriaDto);
+    this.logger.log(
+      'Mensagem enviada a fila no topico criar-categoria',
+      JSON.stringify(criarCategoriaDto),
     );
+  }
+
+  @Get('categorias')
+  consultarCategorias(@Query('idCategoria') _id: string): Observable<any> {
+    this.clientAdminbackend.connect();
+    return this.clientAdminbackend.send('consultar-categorias', _id ? _id : '');
+  }
+
+  @Put('categorias/:_id')
+  @UsePipes(ValidationPipe)
+  async atualizarCategoria(
+    @Body() atualizarCategoriaDto: AtualizarCategoriaDto,
+    @Param('_id') _id: string,
+  ): Promise<void> {
+    this.clientAdminbackend.emit('atualizar-categoria', {
+      id: _id,
+      categoria: atualizarCategoriaDto,
+    });
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as momentTimezone from 'moment-timezone';
 import { AllExceptionsFilter } from './filters/http-exception.filter';
+import { LoggingInterceptor } from './interceptors/loggin.interceptor';
+import { TimeoutInterceptor } from './interceptors/timeout.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalInterceptors(new LoggingInterceptor(), new TimeoutInterceptor());
   //tratamento global de excessão
   app.useGlobalFilters(new AllExceptionsFilter());
   /*
